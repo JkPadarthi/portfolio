77dc5c37d5840aa29bfb9a45d78e622d74226c3c
JkPadarthi <padarthi133@gmail.com>
2025-04-14 20:17:34 +0530

    Simplify education details to show only essential information

--- src/components/commands/Education.tsx
+++ src/components/commands/Education.tsx
@@ -23,51 +23,25 @@ const Date = styled.div`
   margin-bottom: 0.5rem;
 `;
 
-const Details = styled.div`
-  margin-left: 1rem;
-  color: ${({ theme }) => theme.colors.text[200]};
-`;
-
 const Education: React.FC = () => {
   return (
     <EducationContainer>
       <EducationItem>
         <School>Vellore Institute of Technology, Vellore</School>
-        <Degree>Bachelor of Technology in Computer Science and Engineering (AIML)</Degree>
-        <Date>2023 - 2027</Date>
-        <Details>
-          • Currently pursuing B.Tech in CSE with specialization in Artificial Intelligence and Machine Learning
-          <br />
-          • Relevant Coursework: Data Structures and Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, Computer Networks, Software Engineering, Machine Learning, Deep Learning
-          <br />
-          • Active member of the Google Developer Student Clubs (GDSC)
-          <br />
-          • Participated in various hackathons and coding competitions
-        </Details>
+        <Degree>B.Tech in CSE with specialization in AIML</Degree>
+        <Date>Years: 2023 - 2027</Date>
       </EducationItem>
 
       <EducationItem>
         <School>Mathrusri Junior College, Hyderabad</School>
         <Degree>Intermediate Education</Degree>
-        <Date>2021 - 2023</Date>
-        <Details>
-          • Studied Mathematics, Physics, and Chemistry
-          <br />
-          • Achieved excellent academic performance
-          <br />
-          • Participated in various academic competitions
-        </Details>
+        <Date>Years: 2021 - 2023</Date>
       </EducationItem>
 
       <EducationItem>
         <School>The Future Kids School</School>
         <Degree>Secondary Education</Degree>
-        <Date>2021</Date>
-        <Details>
-          • Completed secondary education with distinction
-          <br />
-          • Active participation in extracurricular activities
-        </Details>
+        <Date>Year: 2021</Date>
       </EducationItem>
     </EducationContainer>
   );
